6319a34f5924479c0c6843e2c1071dc4671f7b6d
Altiery <madisonaltiery14@augustana.edu>
2016-09-12 15:42:05 -0500

    Created a class called "Launcher" where the whole program will launch from. This includes the GUI, loading of the CVS documents, etc. Check the comments in the top of the class for more info, and feel free to add to it. Also, I think this should contain our main method, so I added that as well. :]

---
+++ AukletArtistMingle/src/Launcher.java
@@ -0,0 +1,16 @@
+
+public class Launcher {
+
+	/**
+	 * This class will launch the program. So eventually, it will launch the GUI,
+	 * load the CVS documents into arrays in order to load all of the artist data
+	 * and connections, etc. Feel free to add anything else you would like this
+	 * class to do besides in regards to initializing everything.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
