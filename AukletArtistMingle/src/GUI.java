d3a81d0a342c62a0bdeec5025e2064e1f2c89be1
Altiery <madisonaltiery14@augustana.edu>
2016-09-12 16:00:52 -0500

    Added the GUI screen where the user can select if they would like to input a new artist or a new connection.

--- AukletArtistMingle/src/GUI.java
+++ AukletArtistMingle/src/GUI.java
@@ -4,10 +4,22 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GUI implements ActionListener{
+	
+	private JFrame frame;				private JPanel panel1;
+	private	JPanel panel2;				private JPanel panel3;
+	private JPanel panel4;				private JPanel panel5;
+	private JPanel panel6;				private JPanel panel7;
+	private JLabel welcome;				private JButton input;
+	private JButton edit;				private JButton search;
+	private JButton export;				private JButton impt;
+	private JButton exit;				private JFrame frame2;
+	private JLabel selectInputType;		private JButton btnArtist;
+	private JButton btnConnection;
 
 	public GUI() {
-		
-	JFrame frame = new JFrame();
+	
+	// Basic frame setup
+	frame = new JFrame();
 	frame.setBounds(100, 100, 400, 260);
 	frame.setLocationRelativeTo(null);
 	frame.setVisible(true);
@@ -15,26 +27,38 @@ public class GUI implements ActionListener{
 	frame.setLayout(new GridLayout(5,2));
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
-	JPanel panel1 = new JPanel();
-	JPanel panel2 = new JPanel();
-	JPanel panel3 = new JPanel();
-	JPanel panel4 = new JPanel();
-	JPanel panel5 = new JPanel();
+	// Create all panels
+	panel1 = new JPanel();
+	panel2 = new JPanel();
+	panel3 = new JPanel();
+	panel4 = new JPanel();
+	panel5 = new JPanel();
 	
+	// Set the layouts of all of the panels
 	panel1.setLayout(new FlowLayout());
 	panel2.setLayout(new FlowLayout());
 	panel3.setLayout(new FlowLayout());
 	panel4.setLayout(new FlowLayout());
 	panel5.setLayout(new FlowLayout());
 
-	JLabel welcome = new JLabel("Welcome to Artist Mingle");
-	JButton input = new JButton("Input Data");
-	JButton edit = new JButton("Edit/Delete Data");
-	JButton search = new JButton("Search");
-	JButton export = new JButton("Export");
-	JButton impt = new JButton("Import");
-	JButton exit = new JButton("Exit");
+	// Creates components and adds text to them.
+	welcome = new JLabel("Welcome to Artist Mingle");
+	input = new JButton("Input Data");
+	edit = new JButton("Edit/Delete Data");
+	search = new JButton("Search");
+	export = new JButton("Export");
+	impt = new JButton("Import");
+	exit = new JButton("Exit");
 	
+	// Adds action listeners to the buttons
+	input.addActionListener(this);
+	edit.addActionListener(this);
+	search.addActionListener(this);
+	export.addActionListener(this);
+	impt.addActionListener(this);
+	exit.addActionListener(this);
+	
+	// Adds components to panels
 	panel1.add(welcome);
 	panel2.add(input);
 	panel2.add(edit);
@@ -43,6 +67,7 @@ public class GUI implements ActionListener{
 	panel4.add(impt);
 	panel5.add(exit);
 	
+	// Adds panels to frame
 	frame.add(panel1);
 	frame.add(panel2);
 	frame.add(panel3);
@@ -50,10 +75,48 @@ public class GUI implements ActionListener{
 	frame.add(panel5);
 	
 	}
+	
+	/**
+	 * Asks for option of which input the user would like to do.
+	 */
+	public void createGUIAskingImportType() {
+		frame2 = new JFrame();
+		frame2.setBounds(100, 100, 400, 210);
+		frame2.setLocationRelativeTo(null);
+		frame2.setVisible(true);
+		frame2.setTitle("Artist Mingle");
+		frame2.setLayout(new GridLayout(2,1));
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		panel6 = new JPanel();
+		panel7 = new JPanel();
+		
+		panel6.setLayout(new FlowLayout());
+		panel7.setLayout(new FlowLayout());
+		
+		selectInputType = new JLabel("Please select the type of input");
+		btnArtist = new JButton("Artist");
+		btnConnection = new JButton("Connection");
+		
+		panel6.add(selectInputType);
+		panel7.add(btnArtist);
+		panel7.add(btnConnection);
+		
+		frame2.add(panel6);
+		frame2.add(panel7);
+	}
 
+	/**
+	 * Action performed method for buttons
+	 */
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent event) {
+		
+		Object source = event.getSource();
+		
+		if (source == input) {
+			createGUIAskingImportType();
+		}
 		
 	}
 	
